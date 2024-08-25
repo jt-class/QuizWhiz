d63b854e3683739a7a63d2cbfc44028d885eeec6
Albert Lao <119791928+albertlaooo@users.noreply.github.com>
2024-08-25 13:20:45 +0800

    commit

---
+++ QuizWhiz/QuizWhiz/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace QuizWhiz
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ QuizWhiz/QuizWhiz/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace QuizWhiz
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
